c31d2f9bff28721f8c53b2b6c7ef26182ba5ecd2
Spectre <khaos4ng31@gmail.com>
2020-12-05 17:27:58 -0600

    feat(): add part 1

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,37 +1,42 @@
 # day2
 # return number of invalid passwords
 # policy : password
+# part 1: policy = # occur
+# part 2: policy = positions XOR'd for req  
 # `pass.txt`
+# notes at very end explain thought process
 
-# defining classes for purpose of learning Ruby syntax and whatnot
-# yeah I know I could probably do it better but the purpose
-# is to learn Ruby so ¯\_(ツ)_/¯ might as well play with OOP
 
 class Password
-	class Policy
-		@@lowerBound = 0
-		@@upperBound = 0
-		@@requirement = ""
+	PASS_PATTERN = /(?<lower>\d+)-(?<upper>\d+) (?<req>\w): (?<pass>\w+)/
+	@@isValid = true
 
-		def self.lowerBound
-			@@lowerBound
-		end
+	def initialize(line)
+		# input = `lower-upper req: password`
+		m = PASS_PATTERN.match(line)
+		@@lower, @@upper, @@req, @@password = m[1..4]
+		x = @@password.count @@req
+		@@isValid = (@@lower.to_i <= x && x <= @@upper.to_i) ? true : false
+	end
 
-		def self.upperBound
-			@@upperBound
-		end
-
-		def self.requirement
-			@@requirement
-		end
+	def isValid
+		return @@isValid
 	end
 
 end
 
+count = 0
 File.foreach("pass.txt") { 
 	|line| 
-	# ### test
-	# pass.push(line.to_i)  
-	# puts line
-	### test
+	p = Password.new(line)
+	count += p.isValid ? 1:0
 }
+
+puts "Part 1: " + count.to_s
+
+# defining classes was for purpose of learning Ruby syntax and whatnot
+# yeah I know I could probably do it better but the purpose
+# is to learn Ruby so ¯\_(ツ)_/¯ might as well play with OOP
+
+# Could have been finished within `for` loop with regex, counter, lambdas
+# but then I wouldn't have taken the time to learn all of the RB syntax
